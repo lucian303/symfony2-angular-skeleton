91443dbab8a2702bab78f59073fccacecf24b270
Lucian Hontau <lucian303@gmail.com>
2013-07-03 22:00:57 -0700

    Login form showing up correctly.

--- src/Lucian/BgImageBundle/Controller/BgImageController.php
+++ src/Lucian/BgImageBundle/Controller/BgImageController.php
@@ -4,7 +4,10 @@ namespace Lucian\BgImageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Security\Core\SecurityContext;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
