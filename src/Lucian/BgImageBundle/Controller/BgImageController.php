6f1bf76357c6dd069b15bbcc40a22ffb2b14518f
Lucian Hontau <lucian303@gmail.com>
2013-07-03 17:49:04 -0700

    Starting on security

--- src/Lucian/BgImageBundle/Controller/BgImageController.php
+++ src/Lucian/BgImageBundle/Controller/BgImageController.php
@@ -16,8 +16,44 @@ class BgImageController extends Controller
 	 * @Route("/", name="_index")
 	 * @Method("GET")
 	 */
-    public function indexAction()
-    {
-        return $this->render('LucianBgImageBundle:Default:index.html.twig', array('name' => 'world'));
-    }
+	public function indexAction()
+	{
+//		return $this->render('LucianBgImageBundle:Default:index.html.twig', array('name' => 'world'));
+	}
+
+	/**
+	 * @Route("/login", name="_login")
+	 * @Template()
+	 */
+	public function loginAction(Request $request)
+	{
+		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+		}
+		else {
+			$error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+		}
+
+		return array(
+			'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
+			'error' => $error,
+		);
+	}
+
+	/**
+	 * @Route("/login_check", name="_security_check")
+	 */
+	public function securityCheckAction()
+	{
+		// The security layer will intercept this request
+	}
+
+	/**
+	 * @Route("/logout", name="_logout")
+	 */
+	public function logoutAction()
+	{
+		// The security layer will intercept this request
+	}
+
 }
