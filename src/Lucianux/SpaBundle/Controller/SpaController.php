c58512f7f8b6e18271ce060e67e524af00961e17
Lucian Hontau <lucian303@gmail.com>
2013-11-10 07:05:35 -0800

    Cleanup

--- src/Lucianux/SpaBundle/Controller/SpaController.php
+++ src/Lucianux/SpaBundle/Controller/SpaController.php
@@ -3,12 +3,8 @@
 namespace Lucianux\SpaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
-use Symfony\Component\Security\Core\SecurityContext;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -18,13 +14,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  * Class SpaController
  *
  * Single page application controller. Sends out our SPA html layout and handles login
- * @todo Move login / security to a different bundle?
+ *
  * @package Lucianux\SpaBundle\Controller
  */
 class SpaController extends Controller
 {
 	/**
-	 * Forward to login/main app page
+	 * Main SPA app shell page
 	 *
 	 * @Route("/", name="_index")
 	 * @Method("GET")
