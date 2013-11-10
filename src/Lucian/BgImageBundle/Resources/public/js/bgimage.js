156e7acc74e804796dfa515c5917af1aa2eb4e3d
Lucian Hontau <lucian303@gmail.com>
2013-11-09 23:03:24 -0800

    Stripping js. Need to streamline template as html and push it out, then separate the login into its own widget.

--- src/Lucian/BgImageBundle/Resources/public/js/bgimage.js
+++ src/Lucian/BgImageBundle/Resources/public/js/bgimage.js
@@ -1,60 +1,9 @@
 (function($) {
 	"use strict";
 
-	var getInputs = function(el) {
-		var inputs = {};
+	var app = {};
+	app.run = function() {
 
-		$.each(el.find('input'), function(i, item) {
-			var $item = $(item);
-			inputs[$item.attr('name')] = $item.val();
-		});
-
-		return inputs;
-	};
-
-	var renderView = function(id) {
-		var formId = id + '-form';
-		var fileId = id + '-file';
-		$('#app-content').html(
-			Mustache.render(
-				'<form enctype="multipart/form-data" id="{{ formId }}" action="/bgImage" method="POST"><input type="file" name="bgImageFile" id="{{ fileId }}"/><input type="button" id="{{ id }}" value="Upload Background"/></form>',
-				{
-					id: id,
-					formId: formId,
-					fileId: fileId
-				}
-			)
-		);
-	};
-
-	var setEvents = function(id) {
-		$('#' + id).click(function() {
-			var theform = $('#' + id + '-form'),
-				formData = new FormData(theform[0]);
-
-			$.ajax({
-				url: theform.attr('action'),
-				type: 'POST',
-				contentType: false,
-				cache: false,
-				processData: false,
-				data: formData,
-				success: function(model) {
-					if (model.success) {
-						$('body').css('background-image', "url('/bgImage?" + Math.random() + "')");
-					}
-				},
-				error: function() {
-					alert('There was an error uploading the file.');
-				}
-			});
-		});
-	};
-
-	var runApp = function() {
-		var viewId = 'file-upload';
-		renderView(viewId);
-		setEvents(viewId);
 	};
 
 	// Process login and start app once we've logged in
@@ -66,19 +15,19 @@
 				var el = $(e.currentTarget);
 
 				$.ajax({
-					url: el.attr('action'),
-					type: 'POST',
-					dataType: 'json',
-					data: getInputs(el),
-					success: function(data) {
-					   if (data.has_error) {
+			       url: el.attr('action'),
+			       type: 'POST',
+			       dataType: 'json',
+			       data: getInputs(el),
+			       success: function(data) {
+				       if (data.has_error) {
 					       alert('Error: ' + data.error);
-					   }
-					   else {
-					       runApp();
-					   }
-					}
-				});
+				       }
+				       else {
+					       app.run();
+				       }
+			       }
+		       });
 
 				return false;
 			});
