33cda8332b308a7d2ec52a7ef3fd4b4f86e32ff7
Lucian Hontau <lucian303@gmail.com>
2013-11-10 17:55:53 -0800

    Removing form data gathering and using ng-model data to login. Need to figure out why $http doesn't work properly for sending the ajax request.

--- src/Lucianux/SpaBundle/Resources/public/js/controllers.js
+++ src/Lucianux/SpaBundle/Resources/public/js/controllers.js
@@ -6,55 +6,32 @@
 var myloControllers = angular.module('myloControllers', []);
 
 myloControllers.controller('IndexController', ['$scope',
-	function($scope) {
-
-	}
+	function($scope) {	}
 ]);
 
-myloControllers.controller('LoginController', ['$scope', '$http',
-	function($scope, $http) {
+myloControllers.controller('LoginController', ['$http',
+	function($http) {
 		this.username = '';
 		this.password = '';
 		this.url = '/api/v1/login_check';
 
 		/**
-		 * Grabs a form's inputs into an object
-		 *
-		 * @private
-		 * @param el formInput
-		 * @return {{}} output
-		 *
-		 * @todo make this into a service, or better, yet use angular forms
-		 */
-		var getInputs = function(el) {
-			var inputs = {};
-
-			jQuery.each(el.find('input'), function(i, item) {
-				var $item = $(item);
-				inputs[$item.attr('name')] = $item.val();
-			});
-
-			return inputs;
-		};
-
-		/**
-		 * Login action, triggered by a form submit event (usually)
-		 *
 		 * @param $event Form submittal event
-		 *
 		 * @todo refactor out jquery, use $http
 		 */
 		this.loginUser = function loginUser($event) {
-			console.log(this.username, this.password);
-			var loginForm = $($event.target);
+			var loginData = {};
+
+			loginData._username = this.username;
+			loginData._password = this.password;
 
 			$event.preventDefault();
 
 			jQuery.ajax({
-				url: loginForm.attr('action'),
+				url: this.url,
 				type: 'POST',
 				dataType: 'json',
-				data: getInputs(loginForm),
+				data: loginData,
 				success: function($data) {
 				   if ($data.has_error) {
 				       alert('Error: ' + $data.error);
